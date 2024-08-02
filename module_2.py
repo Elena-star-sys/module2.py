ae3bc3d6c3bd2fabf459d54a52cf6f985cd13cd3
Elena-star-sys <elenashulgaelena@gmail.com>
2024-08-02 20:10:35 +0300

    Add files via upload

---
+++ module_2.py
@@ -0,0 +1,20 @@
+# print(123)
+# print(456)
+# print(789)
+
+first = input("Введите число: ")
+second = input("Введите число: ")
+third = input("Введите число: ")
+if first == second == third == first:
+    print(3)
+elif first == second or second == third or third == first:
+     print(2)
+else:
+    first != second != third != first
+    print(0)
+
+
+
+
+
+
